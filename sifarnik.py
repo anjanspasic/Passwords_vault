22f58e116567e25e81917999138a07fd60c765f0
Anja Spasic <anja.n.spasic@gmail.com>
2025-11-15 13:23:54 +0100

    promena2

--- sifarnik.py
+++ sifarnik.py
@@ -1,10 +1,28 @@
-print('1. Unesite aplikaciju i sifru /n 2. Prikazi aplikacije i njihove sifre /n 3.Izmeni sifru /n 4.Obrisi sifru /n 5.Obrisi aplikaciju')
-
-odabir=input('Odaberite sta hocete: ')
-
 sifre={}
 
-if odabir not in ['1','2','3','4','5']:
-    print('Ne moze')
+while True:
 
+    print('1. Unesite aplikaciju i sifru \n 2. Prikazi aplikacije i njihove sifre \n 3.Izmeni sifru \n 4.Obrisi sifru \n 5.Obrisi aplikaciju')
+
+    odabir=input('Odaberite sta hocete: ')
+
+
+    if odabir not in ['1','2','3','4','5']:
+        print('Ne moze')
+
+    if odabir=='1':
+        pass
+
+    if odabir=='2':
+        pass
+
+    if odabir=='3':
+        pass
+
+    if odabir=='4':
+        pass
+
+    if odabir=='5':
+        
+        break
 
