4ee3b68fed34207d87fd9075acfb4ed6fe4349ff
Anja Spasic <anja.n.spasic@gmail.com>
2025-11-15 13:19:36 +0100

    kreirali smo strukturu

---
+++ sifarnik.py
@@ -0,0 +1,10 @@
+print('1. Unesite aplikaciju i sifru /n 2. Prikazi aplikacije i njihove sifre /n 3.Izmeni sifru /n 4.Obrisi sifru /n 5.Obrisi aplikaciju')
+
+odabir=input('Odaberite sta hocete: ')
+
+sifre={}
+
+if odabir not in ['1','2','3','4','5']:
+    print('Ne moze')
+
+
